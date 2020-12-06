fa3164a8e11ebeeb7cc19f67ad1033914d84777a
Piyush Harsh <harh@zhaw.ch>
2020-12-06 08:29:12 +0100

    updated local script

--- scripts/run-locally.sh
+++ scripts/run-locally.sh
@@ -3,6 +3,7 @@
 set -ex
 
 APIHOST=${APIHOST:-api.poc.parvaeres.io}
+MAOHOST=${MAOHOST:-160.85.252.48:8080}
 
 docker run --rm --name parvaeres-ui \
     --tty \
@@ -11,6 +12,7 @@ docker run --rm --name parvaeres-ui \
     --volume "$(pwd)/public:/go/src/github.com/parvaeres/go8s/public" \
     --volume "$(pwd)/app/views:/go/src/github.com/parvaeres/go8s/app/views" \
     --env APIHOST="${APIHOST}" \
+    --env MAOHOST="${MAOHOST}" \
     --env APIVERSION=v1 \
     -p 9000:9000 \
     parvaeres/go8s-ui:latest
